f1022bd692bcb1abba2636b83e41024be22db8df
Will Johnston <wjohnston4@me.com>
2016-10-06 10:20:17 +0100

    Updated Bootstrap file

--- bootstrap_agent.sh
+++ bootstrap_agent.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-Something = "String"
-echo $Something
-
 echo "Minion ready to serve!"
 # get useful packages
 sudo apt-get install -y {vim,tree}
@@ -15,15 +12,13 @@ sudo apt-get install -y puppet
 sudo apt-get update -y
 sudo apt-get install -y puppet 
 
-
-
 # add ip to hosts file
-sudo sed -i "1i/192.168.1.25 swarmlord.qac.local puppetmaster" >> /etc/hosts
-sudo sed -i "2i/127.0.0.1 $(facter fqdn) puppet" >> /etc/hosts
-sudo sed -i "3i/$(facter ipaddress_eth1) $(facter fqdn) puppet" >> /etc/hosts
+sudo sed -i "1i192.168.1.25 swarmlord.qac.local puppetmaster" /etc/hosts
+sudo sed -i "2i127.0.0.1 $(facter fqdn) puppet" /etc/hosts
+sudo sed -i "3i$(facter ipaddress_eth1) $(facter fqdn) puppet" /etc/hosts
 
 # add server to puppet configuration
-sudo sed -i "2i/server=192.168.1.25" >> /etc/puppet/puppet.conf
+sudo sed -i "2iserver=192.168.1.25" /etc/puppet/puppet.conf
 
 # test the puppet server
 sudo puppet agent -t --server=swarmlord.qac.local
