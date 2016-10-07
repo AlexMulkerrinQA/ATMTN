de473f3ee01dbc9f5b8abd540b22228f49397794
AlastairLivingstone <alastair.livingstone@student.anglia.ac.uk>
2016-10-07 14:58:45 +0100

    Added a folder check for nexus

--- Modules/nexus/manifests/init.pp
+++ Modules/nexus/manifests/init.pp
@@ -4,6 +4,6 @@ class nexus
 	{'command when file not exists':
 		provider => shell,
 		command         => '/vagrant/Modules/nexus/files/nexus_installer.sh',
-		#onlyif          => "test ! /opt/java.tar.gz",
+		onlyif          => "test ! /opt/nexus-3.0.2-02",
 	}
 }
