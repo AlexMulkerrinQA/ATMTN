ae601504ca546943f02932b13c9f6802f3e2b5ae
AlastairLivingstone <alastair.livingstone@student.anglia.ac.uk>
2016-10-07 16:10:59 +0100

    Updated mysql module

--- Modules/mysql/files/mysql_installer.sh
+++ Modules/mysql/files/mysql_installer.sh
@@ -1,14 +1,17 @@
 sudo cp /vagrant/IgnoredBinaryFiles/mysql-server_5.7.15-1ubuntu14.04_amd64.deb-bundle.tar  /opt/mysql-server_5.7.15-1ubuntu14.04_amd64.deb-bundle.tar
 cd /opt
-sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/root_password_again password root'
+
+
 sudo apt-get -fy install
 sudo apt-get install libaio1
+sudo apt-get install libmecab2 
+sudo apt-get -fy install
+
 sudo tar -xvf mysql-server_5.7.15-1ubuntu14.04_amd64.deb-bundle.tar
 sudo dpkg -i mysql-common_5.7.15-1ubuntu14.04_amd64.deb
 sudo dpkg -i mysql-community-client_5.7.15-1ubuntu14.04_amd64.deb
 sudo dpkg -i mysql-client_5.7.15-1ubuntu14.04_amd64.deb
-DEBIAN_FRONTEND="noninteractive" sudo dpkg-preconfigure mysql-community-server_5.7.15-1ubuntu14.04_amd64.deb
+sudo DEBIAN_FRONTEND="noninteractive" dpkg-preconfigure mysql-community-server_5.7.15-1ubuntu14.04_amd64.deb
 sudo dpkg -i mysql-community-server_5.7.15-1ubuntu14.04_amd64.deb
 sudo dpkg -i mysql-server_5.7.15-1ubuntu14.04_amd64.deb
 sudo dpkg -i libmysqlclient20_5.7.15-1ubuntu14.04_amd64.deb
