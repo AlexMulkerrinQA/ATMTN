89fe98b7dedec39539bb5f298de91a099ac37996
AlexMulkerrinQA <Alex.Mulkerrin@qa.com>
2016-10-07 15:03:00 +0100

    amend name of exec

--- Modules/nexus/manifests/init.pp
+++ Modules/nexus/manifests/init.pp
@@ -1,7 +1,7 @@
 class nexus 
 {
 	exec 
-	{'command when file not exists':
+	{'install nexus':
 		provider => shell,
 		command         => '/vagrant/Modules/nexus/files/nexus_installer.sh',
 		onlyif          => "test ! /opt/nexus-3.0.2-02",
