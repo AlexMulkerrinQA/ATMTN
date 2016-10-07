0e4fb5b3f4c659db7d7977e442f903d38a1df9c7
AlastairLivingstone <alastair.livingstone@student.anglia.ac.uk>
2016-10-07 11:09:07 +0100

    Made a maven install module

--- Modules/maven/manifests/init.pp
+++ Modules/maven/manifests/init.pp
@@ -1,5 +1,11 @@
 class maven
 {
+	exec {'maven install':
+	provider => shell,
+    command         => '/vagrant/Modules/maven/files/maven_installer.sh',
+    onlyif          => "test ! -f /opt/maven.tar.gz",
+    path            => ['/usr/bin','/usr/sbin','/bin','/sbin'],
+  }
 
-file
+}
 
