76fd328f56da829043ac020a7a7d12f3b8027c16
Dharam-G <Dharam.godhaniya@qa.com>
2016-10-10 15:44:29 +0100

    changes to maven(unfinished) and bamboo modules

--- Modules/maven/files/maven_installer.sh
+++ Modules/maven/files/maven_installer.sh
@@ -3,6 +3,6 @@ cd /opt
 sudo tar zxvf maven.tar.gz
 
 sudo update-alternatives --install /usr/bin/mvn mvn /opt/apache-maven-3.3.9/bin/mvn 100
-sudo update-alternatives --config maven
+sudo update-alternatives --config mvn
 
 export PATH=/opt/apache-maven-3.3.9/bin:$PATH
