3434a24ec86f130e68683efdb8ab8be38be275f0
Dharam-G <Dharam.godhaniya@qa.com>
2016-10-10 12:08:06 +0100

    Bamboo Puppet Module Done.

--- Modules/bamboo/manifests/init.pp
+++ Modules/bamboo/manifests/init.pp
@@ -1,11 +1,29 @@
-class bamboo
-{
-	exec {'bamboo install':
-	provider	=> shell,
-    command	=> '/vagrant/Modules/bamboo/files/install_bamboo.sh',
-    onlyif	=> "test ! -f /opt/atlassian-bamboo-5.13.2.tar.gz",
-    path	=> ['/usr/bin','/usr/sbin','/bin','/sbin'],
-	#require	=> Package['java'],
-  }
-
+class bamboo {
+	
+	file { '/opt/atlassian-bamboo-5.13.2.tar.gz':
+		ensure	=> present,
+		source	=> '/vagrant/IgnoredBinaryFiles/atlassian-bamboo-5.13.2.tar.gz',
+		before	=> Exec['extractBamboo'],
+	}
+	
+	exec { 'extractBamboo':
+		provider	=> shell,
+		onlyif		=> "test ! -f /opt/atlassian-bamboo-5.13.2",
+		cwd			=> '/opt/',
+		command		=> 'sudo tar -zxvf atlassian-bamboo-5.13.2.tar.gz',
+		before		=> Exec['editHomeDirectory'],
+	}
+	
+	exec { 'editHomeDirectory':
+		provider	=> shell,
+		command		=> 'sudo sed -i "1ibamboo.home=C:/Desktop" /opt/atlassian-bamboo-5.13.2/atlassian-bamboo/WEB-INF/classes/bamboo-init.properties',
+		before		=> Exec['startBamboo'],
+	}
+	
+	exec { 'startBamboo':
+		provider	=> shell,
+		cwd			=> '/opt/atlassian-bamboo-5.13.2',
+		command		=> 'sudo bin/start-bamboo.sh'
+		#require		=> service['java'],
+	}
 }
